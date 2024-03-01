1308c5af4942afffb265e3e742bf69be2edbbd0d
zavrgo <80310162+zavrgo@users.noreply.github.com>
2024-03-01 08:36:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,116 @@
 import { ruswords } from "./words.js"
+import { engwords } from "./words.js"
+import { srbwords } from "./words.js"
+
+
+let curLang = "rus"
+let translated = {
+    eng:{
+        mainTitle:"Hangman",
+        checkButton:"Check Letter",
+        newGameButton:"New Game",
+        settingsButton:"Settings",
+        rules:"Type Your Letter And Press Check Letter",
+        rulesInGame:"Used Letters: ",
+        settingsTitle:"Settings",
+        player1Button:"For 1 Player",
+        player2Button:"For 2 Player",
+        langTitle:"Language",
+        multiplayerTitle:"2 Players",
+        multiplayerSubTitle:"the first player comes up with a word, and the second player must guess it",
+        wordInput:"Word",
+        readyButton:"Ready!",
+        winPhrase:"You Wiiin!!!",
+        losePhrase:"You lose, and secret word was a ",
+
+    },
+    crn:{
+        mainTitle:"vjesala",
+        checkButton:"pogledati slovo",
+        newGameButton:"nova igra",
+        settingsButton:"podesavanja",
+        rules:"pisi tvoje slovo i pritisni pogledati slovo",
+        rulesInGame:"koriscena slova: ",
+        settingsTitle:"podesavanja",
+        player1Button:"za 1 covjeka",
+        player2Button:"za 2 covjeka",
+        langTitle:"jezik",
+        multiplayerTitle:"2 covjeka",
+        multiplayerSubTitle:"prvi covjek pise rijec, a drugi treba da to uzna",
+        wordInput:"rijec",
+        readyButton:"gotov!",
+        winPhrase:"ti si pobjedio!!!",
+        losePhrase:"ti si proigrao, sekretno rijec je bilo ",
+
+    },
+    rus:{
+        mainTitle:"виселица",
+        checkButton:"проверить букву",
+        newGameButton:"новая игра",
+        settingsButton:"настройки",
+        rules:"вводи букву и нажимай проверить",
+        rulesInGame:"использованные буквы: ",
+        settingsTitle:"настройки",
+        player1Button:"на 1 игрока",
+        player2Button:"на 2 игрока",
+        langTitle:"язык",
+        multiplayerTitle:"2 игрока",
+        multiplayerSubTitle:"первый игрок загадывает слово, а второй должен его угадать",
+        wordInput:"слово",
+        readyButton:"готово!",
+        winPhrase:"ты победииил!!!",
+        losePhrase:"ты проиграл, а загаданое слово было ",
+    }
+
+
+}
+
+
+ 
+
+function translateGame(lang) {
+    curLang = lang
+h1.innerHTML = translated[lang].mainTitle
+checkButton.innerHTML = translated[lang].checkButton
+newGame.innerHTML = translated[lang].newGameButton
+nastroyki.innerHTML = translated[lang].settingsButton
+podskazka.innerHTML = translated[lang].rules
+setTitle.innerHTML = translated[lang].settingsTitle
+onep.innerHTML = translated[lang].player1Button
+player2.innerHTML = translated[lang].player2Button
+langTitle.innerHTML = translated[lang].langTitle
+multiplayerTitle.innerHTML = translated[lang].multiplayerTitle
+subT.innerHTML = translated[lang].multiplayerSubTitle
+playerInput2.placeholder = translated[lang].wordInput
+wordButton.innerHTML = translated[lang].readyButton
+
+}
+
 
 let checkButton = document.getElementById("checkButton")
 let playerInput = document.getElementById("playerInput")
+let curWords = ruswords
 // выдает рандомное слово
 let secretWord = ruswords[Math.floor(Math.random() * ruswords.length)]
 // secretWord = ()
 let text1 = document.getElementsByClassName("text1")[1]
+let setTitle = document.getElementsByClassName("text1")[0]
+let langTitle = document.getElementById("langTitle")
 let wordButton = document.getElementById("wordButton")
+let multiplayerTitle = document.getElementsByClassName("neZanyat")[0]
+let multiplayerSubTitle = document.getElementById("subT")
 let podskazka = document.getElementById("podskazka")
 let player2 = document.getElementById("player2")
 let playerInput2 = document.getElementById("playerInput2")
 let newShifr = ""
 let img = document.getElementById("img")
 let wordModal = document.getElementById("wordModal")
+let langButtons = document.getElementsByClassName("lang")
 let onep = document.getElementById("onep")
 let nastroyki = document.getElementById("nastroyka")
 let newGame = document.getElementById("newGame")
 let imgnumber = 0
+let singleMode = true
 let modal = document.getElementById("modal")
 let h1 = document.getElementById("h1")
 let letters = []
@@ -26,18 +120,33 @@ letters.pop()
 letters.shift()
 console.log(ruswords);
 
+for (let b of langButtons){
+    b.onclick = function (event){
+        event.preventDefault()
+        let activeButton = document.getElementsByClassName("activeLang")[0]
+        activeButton.classList.remove("activeLang")
+        b.classList.add("activeLang")
+        translateGame(b.id)
+        curWords = b.id == "eng"? engwords: b.id == "crn"? srbwords: ruswords
+        secretWord =  curWords[Math.floor(Math.random() * curWords.length)]
+        start()
+    }
+}
+
+
 // меняет количество черточек в начале
 text1.innerHTML = "_".repeat(secretWord.length)
 
 checkButton.onclick = function (event) {
+    let letter = playerInput.value.toLowerCase()
+
     playerInput.select()
     event.preventDefault()
-    if (!letters.includes(playerInput.value)) {
-        letters.push(playerInput.value)
+    if (!letters.includes(letter)) {
+        letters.push(letter)
     }
-    podskazka.innerHTML = letters
-    console.log(playerInput.value);
-    if (secretWord.includes(playerInput.value)) {
+    podskazka.innerHTML = translated[curLang]["rulesInGame"]+letters
+    if (secretWord.includes(letter)) {
         console.log("yes");
         newShifr = ""
         let lettersAmount = 0
@@ -45,7 +154,7 @@ checkButton.onclick = function (event) {
             if (letters.includes(secretWord[i])) {
                 lettersAmount ++
                 if (lettersAmount == secretWord.length) {
-                    h1.innerHTML = ("ты победил")
+                    h1.innerHTML = translated[curLang]["winPhrase"]
                 }
                 console.log(secretWord[i],lettersAmount);
                 newShifr = newShifr + secretWord[i]
@@ -64,7 +173,7 @@ checkButton.onclick = function (event) {
             checkButton.disabled = true
             checkButton.style.pointerEvents = "none"
             checkButton.style.opacity = 0.5
-            h1.innerHTML = ("ты проиграл, а секретное слово было "+secretWord)
+            h1.innerHTML = translated [curLang]["losePhrase"]+secretWord
 
         }
         img.src = "hangman"+imgnumber+".png"
@@ -86,6 +195,7 @@ player2.onclick = function(event) {
     wordModal.style.pointerEvents = "auto"
     modal.style.opacity = 0
     modal.style.pointerEvents = "none"
+    singleMode = false
 }
 
 wordButton.onclick = function(event) {
@@ -103,6 +213,7 @@ onep.onclick = function(event) {
     event.preventDefault()
     modal.style.opacity = 0
     modal.style.pointerEvents = "none"
+    singleMode = true
 }
 
 
@@ -118,22 +229,31 @@ modal.children[0].onclick = function(event) {
 
 
 
-
-
-
-newGame.onclick = function (event) {
-    event.preventDefault()
-    podskazka.innerHTML = "вводи букву и нажимай проверить"
+function start(){
+    podskazka.innerHTML = translated[curLang]["rules"]
     letters = []
-    h1.innerHTML = ("висилица")
+    h1.innerHTML = curLang =="rus"? "виселица": curLang == "eng"? "hangman": "vjesala"
     playerInput.value = ""
-    secretWord =  ruswords[Math.floor(Math.random() * ruswords.length)]
     text1.innerHTML = "_".repeat(secretWord.length)
     img.src = "hangman"+0+".png"
     imgnumber = 0
     checkButton.disabled = false
     checkButton.style.pointerEvents = "auto"
     checkButton.style.opacity = 1
+}
+
+
+newGame.onclick = function (event) {
+    event.preventDefault()
+    if (singleMode) {
+        secretWord =  curWords[Math.floor(Math.random() * curWords.length)]
+        start()
+    } 
+    else {
+    wordModal.style.opacity = 1
+    wordModal.style.pointerEvents = "auto"
+    wordInput.value = ""
+    }
 }
 
 
